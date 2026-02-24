0aa10ab47e401cdd0724f6f0bc18db6404e5b9f1
danharrow10 <danharrow10@gmail.com>
2026-02-24 13:58:00 +0000

    Signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,41 +1,84 @@
 import { Link } from "react-router-dom";
-import './Signup.css'
+import { useState } from "react";
+import "./Signup.css";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-      const res = fetch("http://localhost:5001/signup", {
-        method:"POST",
-        credentials:"include",
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // prevent page refresh
+
+    try {
+      const res = await fetch("http://localhost:5001/signup", {
+        method: "POST",
+        credentials: "include",
         headers: {
           "Accept": "application/json",
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: email,
-          username: username,
-          password: password,
+          email,
+          username,
+          password,
+        }),
+      });
 
-        })
-      })
-  
-    return(
-        <>
-        <div class="signUpSp1">
-            <div class="signUpBSp">
-                <h1 class="SignUpTit"> Sign Up </h1>
-                <form class="formSp">
-                    <input class="formInp" type="email" id="email" name="email" placeholder="Email"></input>
-                    <input class="formInp" placeholder="Username" type="text" id="username" name="username"></input>
-                    <input class="formInp" placeholder="Password" type="password" id="password" name="password"></input>
-                    <div class="formButtons">
-                        <Link to='/' class="BnSButton"> Back </Link>
-                        <input  class="BnSButton" type="submit" id="submit" name="submit"></input>
-                    </div>
-                </form> 
-            </div>
-        </div>
-        </>
-    )
-};
+      const data = await res.json();
+      console.log("Server response:", data);
+
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
+  return (
+    <div className="signUpSp1">
+      <div className="signUpBSp">
+        <h1 className="SignUpTit">Sign Up</h1>
+
+        <form className="formSp" onSubmit={handleSubmit}>
+          <input
+            className="formInp"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            className="formInp"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+
+          <input
+            className="formInp"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <div className="formButtons">
+            <Link to="/" className="BnSButton">
+              Back
+            </Link>
+
+            <button className="BnSButton" type="submit">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Signup;
